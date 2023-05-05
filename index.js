547ca85284bc09cec082be8f8891cbeff4b39ae8
yunmorenate <mrntfml00@gmail.com>
2023-05-05 16:24:23 +0200

    verify

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const client = new discord.Client({
 		"Guilds",
 		"GuildMessages",
 		"GuildVoiceStates",
-		"MessageContent"
+		"MessageContent",
+		"GuildMembers"
 	]
 })
 const config = require('./config.json')
@@ -26,13 +27,13 @@ const antiSpam = require('./antispam.js')
 //COMPORTAMIENTO BOT CUANDO SE AÑADEN CANCIONES
 client.DisTube.on('playSong', (queue, song) =>
 	queue.textChannel.send(
-		"Now playing: " + song.name + ". Requested by: " + song.user.tag + '\n' + song.url
+		"Now playing: " + song.name + ". Requested by: " + song.user.username + '\n' + song.url
 	)
 )
 
 client.DisTube.on('addSong', (queue, song) =>
 	queue.textChannel.send(
-		"Added to the queue: " + song.name + ". Requested by: " + song.user.tag
+		"Added to the queue: " + song.name + ". Requested by: " + song.user.username
 	)
 )
 
@@ -48,19 +49,25 @@ client.on("messageCreate", message => {
 	antiSpam.spamObj.message(message)
 	const prefix = config.prefix
 
+	//VERIFICACIÓN
+	if (message.channel.id==='1103276078660853840') {
+		if (message.content.toLowerCase() === (config.prefix +"verify")) {
+			message.delete()
+			message.member.roles.add(message.guild.roles.cache.find(r => r.name === 'Verified'))
+			message.member.roles.remove(message.guild.roles.cache.find(r => r.name === 'New'))
+			//SOLO PERMITE ESCRIBIR VERIFY
+		} else {
+			message.delete()
+		}
+
+		return
+	}
+
 	//SI EL MENSAJE NO EMPIEZA POR ASTERISCO SALE DE LA FUNCIÓN, NO ES UN COMANDO
 	if (!message.content.toLowerCase().startsWith(prefix)) return
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/g)
 	let Queue = client.DisTube.getQueue(message)
-
-	if (message.content.toLowerCase() === (config.prefix +"chiste")) {
-		jokes.jokeES(message)
-	}
-
-	if (message.content.toLowerCase() === (config.prefix +"joke")) {
-		jokes.jokeEN(message)
-	}
 
 	//CHECK SI EL USUARIO ESTA EN UN CANAL DE VOZ PARA PERMITIR FUNCIONES DE MUSICA
 	if(message.member.voice.channel) {
@@ -73,6 +80,23 @@ client.on("messageCreate", message => {
 			music.musicQueueFunctions(Queue, message, client.DisTube)
 		}
 	}
+
+	//CHISTES
+	if (message.content.toLowerCase() === (config.prefix +"chiste")) {
+		jokes.jokeES(message)
+	}
+
+	if (message.content.toLowerCase() === (config.prefix +"joke")) {
+		jokes.jokeEN(message)
+	}
+})
+
+//DAR BIENVENIDA Y ROL DE NUEVO A LOS NUEVOS USUARIOS
+client.on('guildMemberAdd', async(member) => {
+	var role= member.guild.roles.cache.find(role => role.name === "New");
+	member.roles.add(role);
+	const welcomeChannel = member.guild.channels.cache.find(c => c.name === 'welcome')
+	welcomeChannel.send('Welcome ' + member.user.username + ' to ' + member.guild.name + '!' + '\n' + 'Please verify yourself in the verification channel.')
 })
 
 client.login(config.token)
